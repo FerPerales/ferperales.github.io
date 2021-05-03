44992d171ae7074aa9358c74fc6ba3795599057e
Fer Perales <me@ferperales.net>
2021-05-02 21:16:41 -0500

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 git checkout gh-pages
+git pull origin gh-pages
+git merge master
 bundle exec middleman build
-mv build docs
+mv -f build docs
+cd docs
+echo 'ferperales.net' > CNAME
+cd ../
 git add .
 git commit -m "New release"
 git push origin gh-pages
+git checkout master
